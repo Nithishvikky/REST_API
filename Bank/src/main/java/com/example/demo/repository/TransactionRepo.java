0c9314728ab25772ed01c11724a8f03cf18f7e41
NITHISH U <108710670+Nithishvikky@users.noreply.github.com>
2023-09-03 18:44:04 +0530

    Delete Bank directory

--- Bank/src/main/java/com/example/demo/repository/TransactionRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Transaction;
-
-public interface TransactionRepo extends JpaRepository<Transaction, Integer>{}
